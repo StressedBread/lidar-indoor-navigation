83dadc8d5095464ecff192e5a082b468c707bac5
StressedBread <martinpolak02@gmail.com>
2026-04-04 13:27:35 +0200

    sector raycast and angles change to use range instead of center and offset

--- LidarIndoorNavigation/Helpers/RiskCalculation.cs
+++ LidarIndoorNavigation/Helpers/RiskCalculation.cs
@@ -10,19 +10,21 @@ namespace LidarIndoorNavigation.Helpers
     {
         int sectors = 20;
         //int distanceCells = 15;
-        float sectorAngle = 0;
-        float startAngle = 0;
+        double sectorAngle = 0;
+        double startAngle = 0;
+        double endAngle = 0;
         double rad = 0;
 
         public double[] EvaluateSectors(int distanceCells)
         {
             double[] risks = new double[sectors];
-            sectorAngle = 240f / sectors;
+            sectorAngle = 240 / sectors;
 
             for (int i = 0; i < sectors; i++)
             {
-                startAngle = -120 + i * sectorAngle + sectorAngle / 2;
-                risks[i] = EvaluateDirection(startAngle, distanceCells);
+                startAngle = -120 + i * sectorAngle;
+                endAngle = startAngle + sectorAngle;
+                risks[i] = EvaluateDirection(startAngle, endAngle, distanceCells);
             }
 
             return risks;
@@ -55,17 +57,14 @@ namespace LidarIndoorNavigation.Helpers
             return risk;
         }*/
 
-        public double EvaluateDirection(float angle, float distanceCells)
+        public double EvaluateDirection(double startSectorAngle, double endSectorAngle, double distanceCells)
         {
-            // cast N rays spread across the 12° sector width
             int rayCount = 5;
-            double sectorHalfWidth = 6.0; // half of 12°
             double totalRisk = 0;
 
             for (int r = 0; r < rayCount; r++)
             {
-                double offset = -sectorHalfWidth + r * (sectorHalfWidth * 2 / (rayCount - 1));
-                double rayAngle = angle + offset;
+                double rayAngle = startSectorAngle + r * ((endSectorAngle - startSectorAngle) / (rayCount - 1));
                 rad = rayAngle * Math.PI / 180;
 
                 double dx = Math.Sin(rad);
@@ -82,7 +81,7 @@ namespace LidarIndoorNavigation.Helpers
                 }
             }
 
-            return totalRisk; 
+            return totalRisk;
         }
     }
 }
